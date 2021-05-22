4bae67faf6e66a9f590ba5ea4ecb393b7f13ffba
nickolai <nickolai.myasnicov@gmail.com>
2021-05-22 18:22:26 +0300

    Swagger with full GET and POST

--- api/schemas/user.py
+++ api/schemas/user.py
@@ -2,10 +2,18 @@ from api import ma
 from api.models.user import UserModel
 
 
-class UserSchema(ma.SQLAlchemySchema):
+class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = UserModel
         fields = ('id', 'username')
+
+
+class UserRequestSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = UserModel
+
+    username = ma.Str()
+    password = ma.Str()
 
 
 user_schema = UserSchema()
